6d8693c6a4b050b25c01cf7ef3e79de1419680b4
afranafrianto-prog <afranafrianto@gmail.com>
2025-11-20 11:53:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function login() {
     let pass = document.getElementById("password").value;
     let error = document.getElementById("errorMsg");
 
-    const correctUser = "siapa namamu";
-    const correctPass = "namaku terkenal";
+    const correctUser = "admin";
+    const correctPass = "12345";
 
     if (user === correctUser && pass === correctPass) {
         window.location.href = "dashboard.html";
     } else {
         error.textContent = "Username atau password salah!";
     }
+}
+
+// ---------------- HALAMAN BERPINDAH TANPA RELOAD ---------------- //
+
+function showPage(pageId) {
+    let pages = document.querySelectorAll(".page");
+
+    pages.forEach(p => p.style.display = "none");
+
+    document.getElementById(pageId).style.display = "block";
 }
